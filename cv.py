11e20f415b035b3060588cd84acce01adf9da3e3
Максимов Максим <maksimovmaksim@MacBook-Pro-von-Maksimov.local>
2024-12-29 22:27:37 +0700

    Add existing file

---
+++ cv.py
@@ -0,0 +1,26 @@
+import mss
+import numpy as np
+import cv2
+
+# Захват экрана
+with mss.mss() as sct:
+    monitor = sct.monitors[1]  # Выбираем основной экран (можно изменить на другой)
+    screenshot = np.array(sct.grab(monitor))  # Получаем скриншот в виде массива
+
+
+img = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (1680, 1360))
+img = cv2.GaussianBlur(img, (1, 1), 0)
+img = cv2.Canny(img,40,40)
+
+
+
+
+
+cv2.imwrite("image_final.png", img)
+print("Обработанное изображение сохранено как processed_image.png")
+
+# Отображение обработанного изображения
+cv2.imshow("image_final", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
