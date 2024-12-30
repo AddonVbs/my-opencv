6d57be44b58ca34ba1721107f2104c765356ecda
Максимов Максим <maksimovmaksim@MacBook-Pro-von-Maksimov.local>
2024-12-30 13:21:00 +0700

    all

--- cv.py
+++ cv.py
@@ -13,6 +13,8 @@ img = cv2.resize(img, (1680, 1360))
 img = cv2.GaussianBlur(img, (1, 1), 0)
 img = cv2.Canny(img,40,40)
 
+contorus, hirevi = cv2.findContours(img, cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+print(f"{len(contorus)} ")
 
 
 
